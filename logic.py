9e33a8b46ea18b417147170301c11f44e2a1908d
Roman Bondarev <robond@ttu.ee>
2018-01-25 18:12:11 +0200

    Update

--- logic.py
+++ logic.py
@@ -5,10 +5,6 @@ DISCLAIMER:
 - Code below is my attempt to recreate game '2048'.
 - Code below is not understandable and clean at all, someday, I hope, it will be :)
 - It is not the correct way how to do it!
-
-If you want to try this yourself, be aware that:
-- if you are using macOS or linux: change self.clear from 'cls' to 'clear'.
-- many 'features' are not implemented yet.
 """
 
 import os
